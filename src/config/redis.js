b35644ade2097455e92b4618f6465bb73e141bab
Aziz Imastara <sazizimastara@gmail.com>
2021-10-05 22:50:24 +0700

    a

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,6 +1,10 @@
 const redis = require("redis");
 
-const client = redis.createClient();
+const client = redis.createClient({
+    host: process.env.REDIS_HOSTNAME,
+    port: process.env.REDIS_PORT,
+    password: process.env.REDIS_PASSWORD
+});
 
 client.on("connect", () => {
   // eslint-disable-next-line no-console
